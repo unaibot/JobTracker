68b401ce29029da7727b423a53c5a5d0e11bc793
unaibot <unaibot@gmail.com>
2017-06-08 18:04:05 +0200

    Show view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,53 @@
 class JobsController < ApplicationController
 
-	def index
+	def index	
+		@jobs = Job.all.order("created_at DESC")	
 	end
+
+	def show
+		
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(job_params)
+		@job.status = "Active"
+			if @job.save
+
+				redirect_to root_path
+			else
+				render 'new'
+			end		
+	end
+
+	def edit
+	end
+
+	def update
+		if @job.update(job_params)
+			redirect_to job_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@job.destroy
+		redirect_to root_path
+	end
+
+	private
+
+		def job_params
+			params.require(:job).permit(:title, :link, :notes)
+		end
+
+		def find_job
+			@job = Job.find(params[:id])
+			
+		end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
  
 	root 'jobs#index'
 	resources :jobs
+	get '/jobs/new', to: 'jobs#new'
+	get '/jobs/show', to: 'jobs#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
